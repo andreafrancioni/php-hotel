26bb0b4b291451434fef75601f7d496e7febeab8
andreafrancioni <andreafrancioni.work@gmail.com>
2023-12-18 17:05:43 +0100

    Crea tabella con bootstrap, ogni riga un hotel con informazioni

--- alberghi.php
+++ alberghi.php
@@ -54,22 +54,31 @@
 
 
     <body>
-        <?php
-        
-        foreach($hotels as $hotel){
-            echo $hotel["name"];
-            echo "<br>";
-            echo $hotel["description"];
-            echo "<br>";
-            echo $hotel["parking"];
-            echo "<br>";
-            echo $hotel["vote"];
-            echo "<br>";
-            echo $hotel["distance_to_center"];
-            echo "<hr>";
-        }
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Hotels</th>
+                </tr>
+            </thead>
+        <tbody>
+            <?php
+            
+            foreach($hotels as $hotel){
 
-        ?>
+                echo "<tr>
+                
+                <td>". $hotel["name"] ."</td>
+                <td>".$hotel["description"]."</td>
+                <td>".$hotel["parking"]."</td>
+                <td>".$hotel["vote"]."</td>
+                <td>".$hotel["distance_to_center"]."</td>
+                </tr>";
+
+            }
+
+            ?>
+        </tbody>
+    </table>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
