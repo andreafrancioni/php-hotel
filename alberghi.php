c6abe062da92ed9bf12dd0f13c4e626e88789476
andreafrancioni <andreafrancioni.work@gmail.com>
2023-12-18 17:47:33 +0100

    Crea pagina di inserimento voto, e filtra gli hotel presenti

--- alberghi.php
+++ alberghi.php
@@ -1,3 +1,7 @@
+<!-- Richieste GET -->
+<?php
+$voto = $_GET["vote"];
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -69,21 +73,23 @@
             <?php
             
             foreach($hotels as $hotel){
-
-                echo "<tr>
-                
-                <td>". $hotel["name"] ."</td>
-                <td>".$hotel["description"]."</td>
-                <td>".$hotel["parking"]."</td>
-                <td>".$hotel["vote"]."</td>
-                <td>".$hotel["distance_to_center"]."</td>
-                </tr>";
+                if($voto == $hotel["vote"]){
+                    echo "<tr>
+                    
+                    <td>". $hotel["name"] ."</td>
+                    <td>".$hotel["description"]."</td>
+                    <td>".$hotel["parking"]."</td>
+                    <td>".$hotel["vote"]."</td>
+                    <td>".$hotel["distance_to_center"]."</td>
+                    </tr>";
+                }
 
             }
 
             ?>
         </tbody>
     </table>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    </body>
+    <a href="ricerca.php">Torna alla pagina di inserimento numero</a>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
---
+++ ricerca.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filtra Hotels</title>
+</head>
+<body>
+    <form action="alberghi.php">
+        <input type="text" placeholder="Inserisci un numero da 1 a 5" name="vote" required>
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
